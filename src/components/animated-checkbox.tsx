c67e1e51eb1bdb455c74196b4053cb5e2c1d16fb
James Daryl Bertiz <jamesdarylbertiz@gmail.com>
2023-07-04 15:17:48 +0800

    Adjust Check through its path, add animatedProps

--- src/components/animated-checkbox.tsx
+++ src/components/animated-checkbox.tsx
@@ -12,7 +12,7 @@ const MARGIN = 15;
 const vWidth = 64 + MARGIN;
 const vHeight = 64 + MARGIN;
 
-const checkmarkPath = 'M15 12.3198 L 15 21 C 34 10 42 -3 50 2.6753';
+const checkmarkPath = 'M15 12.3198 L 15 25 C 40 15 50 0 60 4';
 
 const boxPath =
   'M1.5 25V16C1.5 7.71573 8.21573 1 16.5 1H26.5C34.7843 1 41.5 7.71573 41.5 16V25C41.5 33.2843 34.7843 40 26.5 40H16.5C8.21573 40 1.5 33.2843 1.5 25Z';
@@ -67,6 +67,15 @@ const AnimatedCheckbox = (props: Props) => {
         strokeWidth={7}
         strokeLinejoin="round"
         strokeLinecap="round"
+        animatedProps={animatedBoxProps}
+      />
+
+      <AnimatedPath
+        d={checkmarkPath}
+        strokeWidth={3}
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        animatedProps={animatedBoxProps}
       />
     </Svg>
   );
